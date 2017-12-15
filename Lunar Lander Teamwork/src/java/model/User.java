66ad89a8b113fa196e3e12a844e8dcdb2b25a81b
Andres <>
2017-12-15 20:15:43 +0100

    -	Add JPA Entities from tables. -	Add JPA Controller Classes from Entity Classes -	Add  Context Listener PostgressqlListener. -	Add DataSource pooling connection in context.xml.

---
+++ Lunar Lander Teamwork/src/java/model/User.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author admin
+ */
+@Entity
+@Table(name = "user")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+    , @NamedQuery(name = "User.findById", query = "SELECT u FROM User u WHERE u.id = :id")
+    , @NamedQuery(name = "User.findByName", query = "SELECT u FROM User u WHERE u.name = :name")
+    , @NamedQuery(name = "User.findByUsername", query = "SELECT u FROM User u WHERE u.username = :username")
+    , @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")
+    , @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User u WHERE u.email = :email")})
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userId", fetch = FetchType.LAZY)
+    private List<Configuration> configurationList;
+
+    public User() {
+    }
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    public User(Integer id, String name, String username, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @XmlTransient
+    public List<Configuration> getConfigurationList() {
+        return configurationList;
+    }
+
+    public void setConfigurationList(List<Configuration> configurationList) {
+        this.configurationList = configurationList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "objects.User[ id=" + id + " ]";
+    }
+    
+}
---
+++ Lunar Lander Teamwork/src/java/model/UserJpaController.java
@@ -0,0 +1,219 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 admin.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import model.exceptions.IllegalOrphanException;
+import model.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author admin
+ */
+public class UserJpaController implements Serializable {
+
+    public UserJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(User user) {
+        if (user.getConfigurationList() == null) {
+            user.setConfigurationList(new ArrayList<Configuration>());
+        }
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            List<Configuration> attachedConfigurationList = new ArrayList<Configuration>();
+            for (Configuration configurationListConfigurationToAttach : user.getConfigurationList()) {
+                configurationListConfigurationToAttach = em.getReference(configurationListConfigurationToAttach.getClass(), configurationListConfigurationToAttach.getId());
+                attachedConfigurationList.add(configurationListConfigurationToAttach);
+            }
+            user.setConfigurationList(attachedConfigurationList);
+            em.persist(user);
+            for (Configuration configurationListConfiguration : user.getConfigurationList()) {
+                User oldUserIdOfConfigurationListConfiguration = configurationListConfiguration.getUserId();
+                configurationListConfiguration.setUserId(user);
+                configurationListConfiguration = em.merge(configurationListConfiguration);
+                if (oldUserIdOfConfigurationListConfiguration != null) {
+                    oldUserIdOfConfigurationListConfiguration.getConfigurationList().remove(configurationListConfiguration);
+                    oldUserIdOfConfigurationListConfiguration = em.merge(oldUserIdOfConfigurationListConfiguration);
+                }
+            }
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(User user) throws IllegalOrphanException, NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            User persistentUser = em.find(User.class, user.getId());
+            List<Configuration> configurationListOld = persistentUser.getConfigurationList();
+            List<Configuration> configurationListNew = user.getConfigurationList();
+            List<String> illegalOrphanMessages = null;
+            for (Configuration configurationListOldConfiguration : configurationListOld) {
+                if (!configurationListNew.contains(configurationListOldConfiguration)) {
+                    if (illegalOrphanMessages == null) {
+                        illegalOrphanMessages = new ArrayList<String>();
+                    }
+                    illegalOrphanMessages.add("You must retain Configuration " + configurationListOldConfiguration + " since its userId field is not nullable.");
+                }
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            List<Configuration> attachedConfigurationListNew = new ArrayList<Configuration>();
+            for (Configuration configurationListNewConfigurationToAttach : configurationListNew) {
+                configurationListNewConfigurationToAttach = em.getReference(configurationListNewConfigurationToAttach.getClass(), configurationListNewConfigurationToAttach.getId());
+                attachedConfigurationListNew.add(configurationListNewConfigurationToAttach);
+            }
+            configurationListNew = attachedConfigurationListNew;
+            user.setConfigurationList(configurationListNew);
+            user = em.merge(user);
+            for (Configuration configurationListNewConfiguration : configurationListNew) {
+                if (!configurationListOld.contains(configurationListNewConfiguration)) {
+                    User oldUserIdOfConfigurationListNewConfiguration = configurationListNewConfiguration.getUserId();
+                    configurationListNewConfiguration.setUserId(user);
+                    configurationListNewConfiguration = em.merge(configurationListNewConfiguration);
+                    if (oldUserIdOfConfigurationListNewConfiguration != null && !oldUserIdOfConfigurationListNewConfiguration.equals(user)) {
+                        oldUserIdOfConfigurationListNewConfiguration.getConfigurationList().remove(configurationListNewConfiguration);
+                        oldUserIdOfConfigurationListNewConfiguration = em.merge(oldUserIdOfConfigurationListNewConfiguration);
+                    }
+                }
+            }
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = user.getId();
+                if (findUser(id) == null) {
+                    throw new NonexistentEntityException("The user with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws IllegalOrphanException, NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            User user;
+            try {
+                user = em.getReference(User.class, id);
+                user.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The user with id " + id + " no longer exists.", enfe);
+            }
+            List<String> illegalOrphanMessages = null;
+            List<Configuration> configurationListOrphanCheck = user.getConfigurationList();
+            for (Configuration configurationListOrphanCheckConfiguration : configurationListOrphanCheck) {
+                if (illegalOrphanMessages == null) {
+                    illegalOrphanMessages = new ArrayList<String>();
+                }
+                illegalOrphanMessages.add("This User (" + user + ") cannot be destroyed since the Configuration " + configurationListOrphanCheckConfiguration + " in its configurationList field has a non-nullable userId field.");
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            em.remove(user);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<User> findUserEntities() {
+        return findUserEntities(true, -1, -1);
+    }
+
+    public List<User> findUserEntities(int maxResults, int firstResult) {
+        return findUserEntities(false, maxResults, firstResult);
+    }
+
+    private List<User> findUserEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(User.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public User findUser(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(User.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getUserCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<User> rt = cq.from(User.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
